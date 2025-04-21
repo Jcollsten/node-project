70788a0a9238e69b3f07c923d2bea8a1014e5be5
Jonas <jonas.collsten@gmail.com>
2025-04-21 22:43:05 +0200

    test

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -7,11 +7,9 @@ import logger from '../utils/loggerUtil.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the coworking-space school project API by Jonas' });  
-}
-);
-
+router.use('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the coworking-space school project API by Jonas' });
+});
 
 // Mount user routes
 router.use(
